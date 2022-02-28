c1d008f8d0bc5746e478a1ebb813ef3f1ff208bd
jasconchan <jasonchenzehui@gmail.com>
2022-02-28 22:02:32 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: '/',
+  lang: 'zh-CN',
+  title: '陈泽辉的简历',
+  themeConfig: {
+    sidebar: false,
+    sidebarDepth: 2,
+    editLink: false,
+    lastUpdated: true,
+    lastUpdatedText: '上次更新',
+    contributors: true,
+    contributorsText: '贡献者'
+  },
+  markdown: {
+    code: {
+      lineNumbers: false // 代码块显示行号
+    }
+  }
+}
