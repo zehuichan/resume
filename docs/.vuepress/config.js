cdf786165b9f8f922adeda498c2c6c8f6a27baf1
Zehui Chan <jasonchenzehui@gmail.com>
2022-06-23 16:04:23 +0800

    feat: 依赖升级

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,6 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+const { containerPlugin } = require('@vuepress/plugin-container')
+const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
 const { path } = require('@vuepress/utils')
 
 module.exports = {
@@ -8,8 +11,7 @@ module.exports = {
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },],
   ],
-  theme: path.resolve(__dirname, './theme'),
-  themeConfig: {
+  theme: defaultTheme({
     home: '/',
     repo: 'https://github.com/zehuichan',
     repoLabel: 'Github',
@@ -19,21 +21,15 @@ module.exports = {
     lastUpdatedText: '上次更新',
     contributors: true,
     contributorsText: '贡献者'
-  },
+  }),
   plugins: [
-    [
-      '@vuepress/plugin-container',
-      {
-        type: 'experience-card',
-        before: (info) => `<ExperienceCard title="${info}">\n`,
-        after: () => '</ExperienceCard>\n'
-      }
-    ],
-    [
-      '@vuepress/register-components',
-      {
-        componentsDir: path.resolve(__dirname, './components')
-      }
-    ]
-  ],
+    containerPlugin({
+      type: 'experience-card',
+      before: (info) => `<ExperienceCard title="${info}">\n`,
+      after: () => '</ExperienceCard>\n'
+    }),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, './components')
+    })
+  ]
 }
